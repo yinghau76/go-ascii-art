b548e1e9a5a54cb73f93ce1bff9fd3092d44aeae
Patrick Tsai <yinghau76@gmail.com>
2014-02-18 17:44:41 +0800

    Add a simple test.

---
+++ main_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"image"
+	"testing"
+)
+
+func Test(t *testing.T) {
+	img := image.NewRGBA(image.Rectangle{image.Point{0, 0}, image.Point{160, 160}})
+	art := generateAsciiArt(img)
+	if len(art) != (80+1)*80 {
+		t.Error("Does not generate art with width 80")
+	}
+}
